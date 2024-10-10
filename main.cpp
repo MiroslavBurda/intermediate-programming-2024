6f4bae3d732fb76fa71ec269d6b29b22b7d12d3c
Miroslav Burda <burda.miroslav@gmail.com>
2024-10-10 16:36:31 +0200

    testovaci commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    // std::cout << "hello world" << std::endl;
+    // std::cout << std::boolalpha << 1 + false  << std::endl;
+    std::cout << std::boolalpha << 'a' + 2   << std::endl;
+    return 3; 
+}  

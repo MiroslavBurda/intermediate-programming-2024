2677c3cdcce65cdc7ac10ca9bf1a81751f87c17b
Miroslav Burda <burda.miroslav@gmail.com>
2024-11-09 20:06:56 +0100

    week05 partly resolved

--- week05/excercises.cpp
+++ week05/excercises.cpp
@@ -1,4 +1,6 @@
-// TODO: Doplňte potřebné hlavičky
+#include <cassert>
+#include <iostream>
+#include <cmath>
 
 // Vypište obdélník o zadaných parametrech
 // Vraťte 0 pokud vše v pořádku, -1 pokud nastala chyba
@@ -7,14 +9,53 @@ int hollow_rect(int x, int y, int thickness) {
 }
 
 // Vraťte a na n
-int power(int a, int n) {
-    return 0;
+double power(int a, int n) {    
+    return pow(a, n);
 }
 
 // Vraťte idx-té fibonacciho číslo
 int fibonacci(int idx) {
-    return 0;
+    int fibA = 1;
+    int fibB = 0;
+    int fibC = 0;
+    for (size_t i = 0; i < idx; i++)
+    {
+        fibC = fibA + fibB;
+        fibA = fibB;
+        fibB = fibC; 
+    }
+    
+    return fibC;
 }
 
+int fibRec(int idx) {
+    if (idx < 1)
+        return 0;
+    if ((idx == 1) || (idx == 2)) 
+        return 1;
+
+    return fibRec(idx-1) + fibRec(idx-2);
+}
+
+
 int main() {
+    
+    std::cout << "žiju " << std::endl;
+    std::cout << power(-2, 2) << std::endl;
+    
+    std::cout << "Fib cyklus:"  << std::endl;
+    for (size_t i = 0; i < 8; i++)
+    {
+        std::cout << fibonacci(i) << ", ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Fib rekurze:"  << std::endl;
+    for (size_t i = 0; i < 8; i++)
+    {
+        std::cout << fibRec(i) <<  ", ";
+    }
+    std::cout << std::endl;
+    
+    
 }
